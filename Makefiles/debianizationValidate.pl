98f2e4f21cf341be0ec0d89120d12054a2ce1b2c
Dima Kogan <dima@oblong.com>
2012-08-02 15:04:03 -0700

    debianization validator check more stuff more anally
    More thorough checking. Among other things, I now require each versioned library
    package to Provide an unversioned name. So if the below Package: is present, the
    Provides: must exist as well
    
    Package: liboblong-aaa3.4
    Provides:liboblong-aaa
    
    This is done so that I can rename packages reliably. If I want to rename the
    above package to liboblong-bbb, I may want to conflict with ALL possible
    liboblong-aaa. The unversioned Provides allows this to happen

--- Makefiles/debianizationValidate.pl
+++ Makefiles/debianizationValidate.pl
@@ -1,62 +1,172 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use feature qw(say);
+use Debian::Control;
 
-my %h;
-my $REPO_NAME_ABI_VERSIONED = shift() // die "$0 needs REPO_NAME_ABI_VERSIONED on the cmdline";
+# This script checks the debian/control and debian/changelog files for
+# self-consistency. It also makes sure that the packages defined in
+# debian/control follow the guidelines mandated by the build system.
 
-my ($REPO_NAME,$ABI_VERSION) = $REPO_NAME_ABI_VERSIONED =~ /(.*?)([0-9.]+)$/
-  or die "Couldn't parse repo name, abi version";
 
-open CONTROL, '<', 'debian/control' or die "Couldn't open debian/control";
-while(<CONTROL>)
+my $repo_name_versioned = shift
+  or die "Must get versioned repo name on the commandline";
+
+my ($repo_name, $pkg_version, $abi_version) = getVersionFromChangelog()
+  or die "Couldn't parse versions from debian/changelog";
+
+die <<EOF unless $repo_name_versioned eq "$repo_name$abi_version";
+Makefiles think versioned repo name is '$repo_name_versioned'
+but validation script thinks it's '$repo_name$abi_version'
+EOF
+
+
+
+my ($sourcepkg, $pkgs) = parseControl();
+
+die "debian/control and debian/changelog disagree about the repo name"
+  if $sourcepkg->Source ne $repo_name;
+
+
+
+# go through all the debian/control stanzas, and make sure all the Package names
+# conform to our naming convention
+my %modules;
+for my $pkgname($pkgs->Keys)
 {
-  my @F = split;
+  if( $pkgname =~ /^lib/ )
+  {
+    my $errormsg_base =
+      "Package '$pkgname' doesn't conform to our naming convention of 'liboblong-\$NAME\$ABIVERSION(-dbg|-dev)?':\n";
 
-  if( my ($saw) = /^Source:\s*(.*?)[0-9\.]*$/ )
-  {
-    if( $REPO_NAME ne $saw )
-    {
-      die "Saw incorrect Source version in debian/control.\n" .
-        "Wanted '$REPO_NAME', but saw '$saw'";
-    }
-  }
+    die $errormsg_base . "Library packages must be named 'liboblong-..."
+      unless $pkgname =~ /^liboblong-(.*)/;
 
-  next unless /^Package:/;
+    my $libsuffix = $1;
 
-  my ($pkg, $type);
-  if ( ($pkg, $type) = $F[1] =~ /^((?:lib).+?)$ABI_VERSION(?:-(dbg|dev))?$/ )
-  {
-    $type //= "";
-    $h{$pkg} //= {};
-    $h{$pkg}{$type} = 1;
-  }
-  elsif ( $F[1] =~ /^lib/ )
-  {
-    die "Library $F[1] does not follow convention of libxxxx${ABI_VERSION}{-dev,dbg,} in debian/control";
-  }
-  elsif ( ($pkg, $type) = $F[1] =~ /^(.+?)(?:-(dbg))?$/ )
-  {
-    $type //= "";
-    $h{$pkg} //= {};
-    $h{$pkg}{$type} = 1;
+    my ($name_ver, $type) = $libsuffix =~ /(.*?)(?:-(dbg|dev))?$/;
+    $type ||= 'base';
+
+    my ($name) = $name_ver =~ /(.*)$abi_version$/
+      or die $errormsg_base . "Couldn't find abi_version";
+
+    $name = "lib$name";
+
+    die "Library '$name' of type '$type' defined multiple times in debian/control"
+      if exists $modules{$name}{$type};
+
+    $modules{$name}{$type} = 1;
   }
   else
   {
-    die "This should be unreachable. Please report this as a bug";
+    my $errormsg_base =
+      "Package '$pkgname' doesn't conform to our naming convention of 'oblong-\$NAME(-dbg)?':\n";
+
+    die $errormsg_base . "Non-library packages must be named 'oblong-..."
+      unless $pkgname =~ /^oblong-(.*)/;
+
+    my $suffix = $1;
+
+    my ($name, $type) = $suffix =~ /(.*?)(?:-(dbg))?$/;
+    $type ||= 'base';
+
+    die "Module '$name' of type '$type' defined multiple times in debian/control"
+      if exists $modules{$name}{$type};
+
+    $modules{$name}{$type} = 1;
   }
 }
 
-foreach my $p (keys %h)
+
+# Now make sure that each library has a base a -dev and a -dbg, and that
+# each non-library has a base and a -dbg
+foreach my $name (keys %modules)
 {
-  if ( !defined $h{$p}{""} )
-  {
-    die "Package '$p' doesn't have a base defined in debian/control";
-  }
-  if ( $p =~ /^lib/ && !defined $h{$p}{"dev"} )
-  {
-    die "Package '$p' doesn't have a -dev defined in debian/control";
-  }
+  die "Module $name is missing the base package" if !$modules{$name}{base};
+  die "Module $name is missing the -dbg package" if !$modules{$name}{dbg};
+  die "Module $name is missing the -dev package" if !$modules{$name}{dev} && $name =~ /^lib/;
 }
 
+
+# Make sure each library Provides an unversioned package and that the sections are correct
+for my $pkg($pkgs->Values)
+{
+  my $name = $pkg->Package;
+  if( $name =~ /^lib/ )
+  {
+    if( my ($unversioned, $type) =
+        $name =~ /^(liboblong-.*?)$abi_version(?:-(dbg|dev))?$/ )
+    {
+      if( $type )
+      {
+        $unversioned .= "-$type";
+      }
+
+      die "$name: -dev packages must be in Section libdevel"
+        if $type && $type eq 'dev' && $pkg->Section ne 'libdevel';
+
+      die "$name: -dbg packages must be in Section debug"
+        if $type && $type eq 'dbg' && $pkg->Section ne 'debug';
+
+      my %provides = map {$_ => 1} @{$pkg->Provides()};
+      die "$name: Each versioned library must Provide the unversioned name"
+        unless $provides{$unversioned};
+    }
+    else
+    {
+      die "Error in $0. Please report this as a bug";
+    }
+  }
+}
 exit 0;
+
+
+
+
+
+
+
+
+sub getVersionFromChangelog
+{
+  open F, '<', 'debian/changelog' or die "Couldn't open debian/changelog";
+
+  # First line should be something like
+  #
+  # buildsystem-unittests (5.6.7) unstable; urgency=low
+  #
+  # I grab the repo name and the version number;
+  <F> =~ /^(\S+) \s* # repo name
+          \((.*?)\)  # version
+         /x or die "Couldn't parse debian/changelog";
+
+  my $name        = $1;
+  my $pkg_version = $2;
+
+  # version number is some number of tokens separated by '.', for example:
+  # a.b.c.d.e.f
+  # All but the last token are treated as the ABI version, so in this case,
+  # everything except the '.f' is the ABI version
+  my ($abi_version) = $pkg_version =~ /(.*)\..*?$/;
+
+  close F;
+  return ($name, $pkg_version, $abi_version);
+}
+
+sub parseControl
+{
+  # There's an issue with Debian::Control where comments in debian/control
+  # confuse it. I thus read the file myself, strip the comments, and pass that
+  # data on to Debian::Control
+  open F, '<', 'debian/control' or die "Couldn't open debian/control";
+  my @lines = <F>;
+  close F;
+
+  my $control = join('', grep !/^#/, @lines);
+
+  my $c = Debian::Control->new;;
+  eval { $c->read( \$control ) };
+  die "Error parsing debian/control: $@" if $@;
+
+  return ($c->source, $c->binary);
+}
