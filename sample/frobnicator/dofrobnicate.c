e34819774ed9d42f6bf665963b00046e2f2076c0
Dima Kogan <dima@secretsauce.net>
2012-10-30 00:41:51 -0700

    added sample project

---
+++ sample/frobnicator/dofrobnicate.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "libAAA/aaa.h"
+
+int main(void)
+{
+  aaa();
+  printf("frobnicating\n");
+
+  return 0;
+}
---
+++ sample/libAAA/aaa.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void aaa(void)
+{
+  printf("aaa\n");
+}
